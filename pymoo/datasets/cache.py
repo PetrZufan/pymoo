28598c9c182c367935a3734e955bc848c7a6fd89
Petr Žufan <petr.zufan@smarteon.cz>
2021-10-05 13:59:14 +0200

    introduce DIBCO2009 dataset

---
+++ pymoo/datasets/cache.py
@@ -0,0 +1,83 @@
+import os
+import pickle
+
+
+class Cache:
+    cache_dir = ".cache"
+    content_file = "content.p"
+
+    def __init__(self, dataset_dir):
+        self.dataset_dir = self._check_dataset_dir(dataset_dir)
+        if self.dataset_dir is None:
+            raise Exception(str(dataset_dir) + " directory not found")
+        self.cache_dir = os.path.join(self.dataset_dir, Cache.cache_dir)
+        self.content_file = os.path.join(self.cache_dir, Cache.content_file)
+        self._check_cache_files()
+
+    def save(self, data, file):
+        self._add_content(file)
+        cache_file = os.path.join(self.cache_dir, file)
+        pickle.dump(data, open(cache_file, "wb"))
+
+    def load(self, file):
+        cache_file = os.path.join(self.cache_dir, file)
+        if self._check_content(file):
+            return pickle.load(open(cache_file, "rb"))
+        else:
+            return None
+
+    def clear(self):
+        pickle.dump([], open(self.content_file, "wb"))
+
+    def _check_cache_files(self):
+        if not os.path.isdir(self.cache_dir):
+            os.mkdir(self.cache_dir)
+        if not os.path.isfile(self.content_file):
+            pickle.dump([], open(self.content_file, "wb"))
+
+    def _get_content(self):
+        if not os.path.isfile(self.content_file):
+            return []
+        else:
+            return pickle.load(open(self.content_file, "rb"))
+
+    def _set_content(self, data):
+        pickle.dump(data, open(self.content_file, "wb"))
+
+    def _add_content(self, data):
+        cache = self._get_content()
+        if not self._check_content(data):
+            cache.append(data)
+            pickle.dump(cache, open(self.content_file, "wb"))
+
+    def _remove_content(self, data):
+        cache = self._get_content()
+        keep = []
+        for i, item in enumerate(cache):
+            if item != data:
+                keep.append(i)
+        pickle.dump(cache[keep], open(self.content_file, "wb"))
+
+    def _clear_content(self):
+        pickle.dump([], open(self.content_file, "wb"))
+
+    def _check_content(self, data):
+        cache = self._get_content()
+        for item in cache:
+            if item == data:
+                return True
+        return False
+
+    def _check_dataset_dir(self, dataset_dir):
+        if os.path.isdir(dataset_dir):
+            return dataset_dir
+        new_dir = os.path.join("../..", dataset_dir)
+        if os.path.isdir(new_dir):
+            return new_dir
+        new_dir = os.path.join("datasets", dataset_dir)
+        if os.path.isdir(new_dir):
+            return new_dir
+        new_dir = os.path.join("../../datasets", dataset_dir)
+        if os.path.isdir(new_dir):
+            return new_dir
+        return None
---
+++ pymoo/datasets/dibco.py
@@ -0,0 +1,195 @@
+
+import numpy as np
+import os
+import tensorflow as tf
+
+from pymoo.datasets.cache import Cache
+
+
+class DIBCO:
+
+    _directory_dataset = 'datasets/dibco2009'
+    _directory_in = _directory_dataset + '/DIBC02009_Test_images-handwritten'
+    _directory_out = _directory_dataset + '/DIBCO2009-GT-Test-images_handwritten_bmp'
+
+    def __init__(self):
+        self._directory_dataset = self.check_dirname(DIBCO._directory_dataset)
+        self._directory_in = self.check_dirname(DIBCO._directory_in)
+        self._directory_out = self.check_dirname(DIBCO._directory_out)
+        self._cache = Cache(self._directory_dataset)
+
+    def load_sampled(self, grid_size=25, use_cache=True):
+        tr_file = "H05.bmp"
+        in_tr = self.load_sampled_in_one(tr_file, grid_size, use_cache)
+        out_tr = self.load_sampled_out_one(tr_file, grid_size, use_cache)
+        ts_file = "H04.bmp"
+        in_ts = self.load_sampled_in_one(ts_file, grid_size, use_cache)
+        out_ts = self.load_sampled_out_one(ts_file, grid_size, use_cache)
+        return in_tr, out_tr, in_ts, out_ts
+
+    def load_sampled_in_first(self, count=1, grid_size=25, use_cache=True):
+        images = []
+        i = 0
+        for file in os.listdir(self._directory_in):
+            image = self.load_sampled_in_one(file, grid_size, use_cache)
+            images.append(image)
+            i += 1
+            if i >= count:
+                break
+        return images
+
+    def load_sampled_out_first(self, count=1, grid_size=25, use_cache=True):
+        images = []
+        i = 0
+        for file in os.listdir(self._directory_in):
+            image = self.load_sampled_out_one(file, grid_size, use_cache)
+            images.append(image)
+            i += 1
+            if i >= count:
+                break
+        return images
+
+    def load_sampled_in_all(self, grid_size=25, use_cache=True):
+        images = []
+        for file in os.listdir(self._directory_in):
+            image = self.load_sampled_in_one(file, grid_size, use_cache)
+            images.append(image)
+        return images
+
+    def load_sampled_out_all(self, grid_size=25, use_cache=True):
+        images = []
+        for file in os.listdir(self._directory_out):
+            image = self.load_sampled_out_one(file, grid_size, use_cache)
+            images.append(image)
+        return images
+
+    def load_sampled_in_one(self, file, grid_size=25, use_cache=True):
+        return self.load_sampled_one(file, "in", grid_size, use_cache)
+
+    def load_sampled_out_one(self, file, grid_size=25, use_cache=True):
+        return self.load_sampled_one(file, "out", grid_size, use_cache)
+
+    def load_sampled_one(self, file, inout, grid_size=25, use_cache=True):
+        postfix = "_s" + str(grid_size)
+        if inout == "in":
+            postfix = postfix + "_in.p"
+        else:
+            postfix = postfix + "_out.p"
+
+        cache_file = file + postfix
+        if use_cache:
+            sampled = self._cache.load(cache_file)
+            if sampled is not None:
+                return sampled
+
+        image = self.load_one(file, inout, use_cache)
+        sampled = self.sample(image, grid_size)
+
+        self._cache.save(sampled, cache_file)
+
+    def sample(self, img, grid_size=25):
+        padding = int(grid_size / 2)
+        pad = tf.constant([[padding, padding], [padding, padding]])
+        padded_img = tf.pad(img, pad, 'CONSTANT', constant_values=0)
+
+        samples = np.zeros([img.shape[0]*img.shape[1], grid_size, grid_size])
+        for i in np.arange(img.shape[0]):
+            for j in np.arange(img.shape[1]):
+                s = padded_img[i:i+grid_size, j:j+grid_size]
+                samples[i*img.shape[1]+j] = s
+        return samples
+
+    # -----------------------------------
+
+    def load_in_all(self, use_cache=True):
+        images = []
+        for file in os.listdir(self._directory_in):
+            image = self.load_in_one(file, use_cache)
+            images.append(image)
+        return images
+
+    def load_out_all(self, use_cache=True):
+        images = []
+        for file in os.listdir(self._directory_out):
+            image = self.load_out_one(file, use_cache)
+            images.append(image)
+        return images
+
+    def load_in_one(self, file, use_cache=True):
+        return self.load_one(file, "in", use_cache)
+
+    def load_out_one(self, file, use_cache=True):
+        return self.load_one(file, "out", use_cache)
+
+    def load_one(self, file, inout, use_cache=True):
+        if inout == "in":
+            postfix = "_in.p"
+            directory = self._directory_in
+        else:
+            postfix = "_out.p"
+            directory = self._directory_out
+
+        cache_file = file + postfix
+        if use_cache:
+            image = self._cache.load(cache_file)
+            if image is not None:
+                return image
+
+        file_path = os.path.join(directory, file)
+        image = self.load_bmp(file_path)
+
+        self._cache.save(image, cache_file)
+        return image
+
+    # --------------------------------------
+
+    def load_in_first(self):
+        return self.load_bmp('datasets/dibco2009/DIBC02009_Test_images-handwritten/H01.bmp')
+
+    def load_out_first(self):
+        return self.load_bmp('datasets/dibco2009/DIBCO2009-GT-Test-images_handwritten_bmp/H01.bmp')
+
+    def load_bmp(self, filename):
+        filename = self.check_filename(filename)
+        if filename is None:
+            return None
+        bmp_file = tf.io.read_file(filename)
+        image = tf.image.decode_bmp(bmp_file)
+        return image[:, :, 0]
+
+    def check_filename(self, filename):
+        if os.path.isfile(filename):
+            return filename
+        new_filename = os.path.join("../..", filename)
+        if os.path.isfile(new_filename):
+            return new_filename
+        new_filename = os.path.join("datasets", filename)
+        if os.path.isfile(new_filename):
+            return new_filename
+        new_filename = os.path.join("../../datasets", filename)
+        if os.path.isfile(new_filename):
+            return new_filename
+        return None
+
+    def check_dirname(self, dirname):
+        if os.path.isdir(dirname):
+            return dirname
+        new_dirname = os.path.join("../..", dirname)
+        if os.path.isdir(new_dirname):
+            return new_dirname
+        new_dirname = os.path.join("datasets", dirname)
+        if os.path.isdir(new_dirname):
+            return new_dirname
+        new_dirname = os.path.join("../../datasets", dirname)
+        if os.path.isdir(new_dirname):
+            return new_dirname
+        return None
+
+    # ------------------------------------
+
+
+if __name__ == "__main__":
+    dataset = DIBCO()
+    # in_tr, out_tr, in_ts, out_ts = dataset.load_sampled()
+    in_tr = dataset.load_sampled_in_one("H05.bmp")
+    print("end")
