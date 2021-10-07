ff693be4c2f4d85aa8fd16e25446bcf1a8455904
Petr Žufan <petr.zufan@smarteon.cz>
2021-10-07 12:55:08 +0200

    make padding parametrized

--- pymoo/datasets/dibco.py
+++ pymoo/datasets/dibco.py
@@ -18,59 +18,61 @@ class DIBCO:
         self._directory_out = self.check_dirname(DIBCO._directory_out)
         self._cache = Cache(self._directory_dataset)
 
-    def load_sampled(self, grid_size=25, use_cache=True):
+    def load_sampled(self, grid_size=25, use_cache=True, add_padding=False):
         tr_file = "H05.bmp"
-        in_tr = self.load_sampled_in_one(tr_file, grid_size, use_cache)
-        out_tr = self.load_sampled_out_one(tr_file, grid_size, use_cache)
+        in_tr = self.load_sampled_in_one(tr_file, grid_size, use_cache, add_padding)
+        out_tr = self.load_sampled_out_one(tr_file, grid_size, use_cache, add_padding)
         ts_file = "H04.bmp"
-        in_ts = self.load_sampled_in_one(ts_file, grid_size, use_cache)
-        out_ts = self.load_sampled_out_one(ts_file, grid_size, use_cache)
+        in_ts = self.load_sampled_in_one(ts_file, grid_size, use_cache, add_padding)
+        out_ts = self.load_sampled_out_one(ts_file, grid_size, use_cache, add_padding)
         return in_tr, out_tr, in_ts, out_ts
 
-    def load_sampled_in_first(self, count=1, grid_size=25, use_cache=True):
+    def load_sampled_in_first(self, count=1, grid_size=25, use_cache=True, add_padding=False):
         images = []
         i = 0
         for file in os.listdir(self._directory_in):
-            image = self.load_sampled_in_one(file, grid_size, use_cache)
+            image = self.load_sampled_in_one(file, grid_size, use_cache, add_padding)
             images.append(image)
             i += 1
             if i >= count:
                 break
         return images
 
-    def load_sampled_out_first(self, count=1, grid_size=25, use_cache=True):
+    def load_sampled_out_first(self, count=1, grid_size=25, use_cache=True, add_padding=False):
         images = []
         i = 0
         for file in os.listdir(self._directory_in):
-            image = self.load_sampled_out_one(file, grid_size, use_cache)
+            image = self.load_sampled_out_one(file, grid_size, use_cache, add_padding)
             images.append(image)
             i += 1
             if i >= count:
                 break
         return images
 
-    def load_sampled_in_all(self, grid_size=25, use_cache=True):
+    def load_sampled_in_all(self, grid_size=25, use_cache=True, add_padding=False):
         images = []
         for file in os.listdir(self._directory_in):
-            image = self.load_sampled_in_one(file, grid_size, use_cache)
+            image = self.load_sampled_in_one(file, grid_size, use_cache, add_padding)
             images.append(image)
         return images
 
-    def load_sampled_out_all(self, grid_size=25, use_cache=True):
+    def load_sampled_out_all(self, grid_size=25, use_cache=True, add_padding=False):
         images = []
         for file in os.listdir(self._directory_out):
-            image = self.load_sampled_out_one(file, grid_size, use_cache)
+            image = self.load_sampled_out_one(file, grid_size, use_cache, add_padding)
             images.append(image)
         return images
 
-    def load_sampled_in_one(self, file, grid_size=25, use_cache=True):
-        return self.load_sampled_one(file, "in", grid_size, use_cache)
+    def load_sampled_in_one(self, file, grid_size=25, use_cache=True, add_padding=False):
+        return self.load_sampled_one(file, "in", grid_size, use_cache, add_padding)
 
-    def load_sampled_out_one(self, file, grid_size=25, use_cache=True):
-        return self.load_sampled_one(file, "out", grid_size, use_cache)
+    def load_sampled_out_one(self, file, grid_size=25, use_cache=True, add_padding=False):
+        return self.load_sampled_one(file, "out", grid_size, use_cache, add_padding)
 
-    def load_sampled_one(self, file, inout, grid_size=25, use_cache=True):
+    def load_sampled_one(self, file, inout, grid_size=25, use_cache=True, add_padding=False):
         postfix = "_s" + str(grid_size)
+        if add_padding:
+            postfix = postfix + "p"
         if inout == "in":
             postfix = postfix + "_in.p"
         else:
@@ -83,20 +85,27 @@ class DIBCO:
                 return sampled
 
         image = self.load_one(file, inout, use_cache)
-        sampled = self.sample(image, grid_size)
+        sampled = self.sample(image, grid_size, add_padding)
 
         self._cache.save(sampled, cache_file)
 
-    def sample(self, img, grid_size=25):
+    def sample(self, img, grid_size=25, add_padding=False):
         padding = int(grid_size / 2)
-        pad = tf.constant([[padding, padding], [padding, padding]])
-        padded_img = tf.pad(img, pad, 'CONSTANT', constant_values=0)
+        if add_padding:
+            shape_x = img.shape[0]
+            shape_y = img.shape[1]
+            pad = tf.constant([[padding, padding], [padding, padding]])
+            final_img = tf.pad(img, pad, 'CONSTANT', constant_values=0)
+        else:
+            shape_x = img.shape[0] - grid_size
+            shape_y = img.shape[1] - grid_size
+            final_img = img
 
-        i, j = tf.meshgrid(np.arange(img.shape[0]), np.arange(img.shape[1]), indexing="ij")
+        i, j = tf.meshgrid(np.arange(shape_x), np.arange(shape_y), indexing="ij")
         i = tf.reshape(i, [i.shape[0] * i.shape[1]])
         j = tf.reshape(j, [j.shape[0] * j.shape[1]])
         samples = tf.map_fn(
-            lambda x: padded_img[x[0]:x[0]+grid_size, x[1]:x[1]+grid_size],
+            lambda x: final_img[x[0]:x[0] + grid_size, x[1]:x[1] + grid_size],
             tf.stack([i, j], axis=1),
             dtype=tf.uint8
         )
