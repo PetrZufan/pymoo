e4bab1f30f88378ff67dbc788f198e93528f113b
Petr Žufan <petr.zufan@smarteon.cz>
2021-10-12 15:58:55 +0200

    save results to result dir

--- main/results.py
+++ main/results.py
@@ -6,10 +6,13 @@ import random
 class Results:
     ps = os.getpid()
     rnd = random.randint(0, 100)
-    folder = "./" + str(ps) + "_" + str(rnd)
+    result_folder = "./results"
+    folder = result_folder + "/" + str(ps) + "_" + str(rnd)
 
     def __init__(self, folder=None):
         self.folder = Results.folder if folder is None else folder
+        if not os.path.isdir(Results.result_folder):
+            os.mkdir(Results.result_folder)
         if not os.path.isdir(self.folder):
             os.mkdir(self.folder)
 
